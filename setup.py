db8a04626a17eadf7be5eb6fc60c3b34aeeb0270
shk305 <31530095+shk305@users.noreply.github.com>
2020-05-27 02:15:21 -0500

    test commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='This project uses a convolutional neural network model to guess a dog's breed from an image of the dog. ',
+    description="This project uses a convolutional neural network model to guess a dog's breed from an image of the dog. ",
     author='Sharjil Khan',
     license='MIT',
 )
